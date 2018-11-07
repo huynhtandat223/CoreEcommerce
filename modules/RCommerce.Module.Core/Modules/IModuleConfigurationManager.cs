48ed041bfc6ec4754e2f8f55ae3287c84c01ee5a
Dat <kaka391989@gmail.com>
2018-11-07 19:22:46 +0700

    research solution.

--- modules/RCommerce.Module.Core/Modules/IModuleConfigurationManager.cs
+++ modules/RCommerce.Module.Core/Modules/IModuleConfigurationManager.cs
@@ -3,17 +3,22 @@ using RCommerce.Module.Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
 
 namespace RCommerce.Module.Core.Modules
 {
     public interface IModuleConfigurationManager
     {
         IEnumerable<ModuleInfo> GetModules();
+        IEnumerable<ModuleInfo> GetModules(string moduleFolder);
+
     }
 
     public class ModuleConfigurationManager : IModuleConfigurationManager
     {
-        public static readonly string ModulesFilename = "modules.json";
+        public static readonly string ModulesFilename = "module.json";
 
         public IEnumerable<ModuleInfo> GetModules()
         {
@@ -31,6 +36,18 @@ namespace RCommerce.Module.Core.Modules
                     };
                 }
             }
+        }
+
+        public IEnumerable<ModuleInfo> GetModules(string moduleFolder)
+        {
+            return Directory.GetFiles(moduleFolder)
+                .Select(i => new ModuleInfo {
+                    Id = Path.GetFileName(i),
+                    IsBundledWithHost = false,
+                    Version = Version.Parse("1.0"),
+                    Name = i,
+                    Assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(i)
+                });
         }
     }
 }
